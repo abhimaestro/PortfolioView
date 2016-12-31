e7b88c9ffc46b259402c290705b2c63d52e52367
abhimaestro <abhishek.sharma@algolic.com>
2016-12-31 12:24:36 -0500

    login screen

---
+++ PortfolioView/DisclaimerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DisclaimerViewController.swift
+//  PortfolioView
+//
+//  Created by Abhishek Sharma on 12/31/16.
+//  Copyright © 2016 Abhishek Sharma. All rights reserved.
+//
+
+import UIKit
+
+class DisclaimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PortfolioView/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  PortfolioView
+//
+//  Created by Abhishek Sharma on 12/31/16.
+//  Copyright © 2016 Abhishek Sharma. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToLogin(unwindSegue: UIStoryboardSegue) {
+        
+    }
+}
